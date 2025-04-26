f987a02516816d19cb5aadefb4888d14f5947e97
Artenlaclase <artenlaclase@gmail.com>
2025-04-25 23:20:12 -0400

    Update project structure: refine .gitignore, add Astro config, and create initial HTML, CSS, and SVG files for the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  base: '/cartamagica/', // Cambia esto por el nombre de tu repositorio
+  trailingSlash: 'ignore',
   integrations: [],
   vite: {
     css: {
